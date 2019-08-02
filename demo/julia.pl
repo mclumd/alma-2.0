d00ef4843f70746bf3fd930d661bfa5fc67436d9
Jesse Silverberg <jessesilverberg11@gmail.com>
2019-08-02 14:13:54 -0400

    Points to a person

--- demo/julia.pl
+++ demo/julia.pl
@@ -1,6 +1,12 @@
 fif(
     and(hearing(julia, T),
     and(proc(neg_int(talking),bound),
-    proc(pos_int(now(X)), bound))),
+    and(proc(pos_int(now(X)), bound),
+    proc(pos_int(saw(person,A,B,C,D,E,F)), bound)))),
+    conclusion(ros(raise_arm(B,C), X))).
+fif(
+    and(hearing(julia, T),
+    and(proc(neg_int(talking),bound),
+    and(proc(pos_int(now(X)), bound),
+    proc(pos_int(saw(person,A,B,C,D,E,F)), bound)))),
     conclusion(ros(speak(), X))).
-hearing(julia, 0).
