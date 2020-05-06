4d18dbb5b444c0f1c79519dbcf001811527ebbca
Justin Brody <jdbrody@gmail.com>
2020-05-06 19:56:12 -0400

    adding clause.h to repo

---
+++ clause.h
@@ -0,0 +1,22 @@
+#ifndef clause_h
+#define clause_h
+#include "alma_formula.h"
+
+typedef struct clause {
+  int pos_count;
+  int neg_count;
+  alma_function **pos_lits;
+  alma_function **neg_lits;
+  int parent_set_count;
+  int children_count;
+  struct parent_set *parents; // Use more efficient structure for as needed
+  struct clause **children; // Use more efficient structure for as needed
+  if_tag tag;
+  struct fif_info *fif; // Data used to store additional fif information; non-null only if FIF tagged
+  long index; // Index of clause, used as key in index_map of KB
+  long learned; // Time asserted to KB
+} clause;
+
+
+
+#endif
