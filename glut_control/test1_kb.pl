fc351ceb97624f12d0225eac45251a9527aca4c4
jdbrody <justin.brody@goucher.edu>
2020-10-01 18:50:07 -0400

    glut control

---
+++ glut_control/test1_kb.pl
@@ -0,0 +1,4 @@
+%if(and(distanceAt(Item1, D1, T), distanceAt(Item2, D2, T)), distanceBetweenBoundedBy(Item1, Item2, D1, T)).
+%if(and(distanceAt(b, D1, T), distanceAt(Item2, D2, T)), distanceBetweenBoundedBy(Item2, b, D1, T)).
+ if(and(distanceAt(Item2, D2, T), distanceAt(b, D1, T)), distanceBetweenBoundedBy(Item2, b, D1, T)).
+
