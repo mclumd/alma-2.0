b406b130d515fcc6bbdaccbfaf8e4ceb9671dcdd
jdbrody <justin.brody@goucher.edu>
2021-01-22 10:56:57 -0500

    Tweaking for first January experiment

--- glut_control/test1.py
+++ glut_control/test1.py
@@ -8,7 +8,7 @@ Collect data and report results.
 
 import random
 import os
-import rl_utils as rl_functions
+import resolution_prebuffer as rl_functions
 import numpy as np
 import itertools
 import pickle
@@ -22,7 +22,7 @@ test_params = {
     'explosion_size': 1000,
     'alma_heap_print_size': 100
 }
-alma_inst,res = alma.init(1,'/home/justin/alma-2.0/glut_control/test1_kb.pl', '0', 1, 1000, [], [])
+#alma_inst,res = alma.init(1,'/home/justin/alma-2.0/glut_control/test1_kb.pl', '0', 1, 1000, [], [])
 
 def res_task_lits(lit_str):
     L = lit_str.split('\n')[:-1]
@@ -46,7 +46,7 @@ def explosion(size=1000):
     print("="*80)
     return r
 
-def train(explosion_steps=50, num_steps=500, numeric_bits=10, model_name="test1"):
+def train(explosion_steps=50, num_steps=500, numeric_bits=10, model_name="test1", axiom_file="test1_kb.pl", num_rounds=10000):
     global alma_inst,res
     subjects = []
     for place in range(3):
@@ -55,9 +55,9 @@ def train(explosion_steps=50, num_steps=500, numeric_bits=10, model_name="test1"
         for num_subj in range(2**numeric_bits):
             subjects.append("{}/{}".format(num_subj, place))
     network = rl_functions.res_prefilter(subjects, [])
-    for b in range(10000):
+    for b in range(num_rounds):
         print("Starting round ", b)
-        alma_inst,res = alma.init(1,'/home/justin/alma-2.0/glut_control/test1_kb.pl', '0', 1, 1000, [], [])
+        alma_inst,res = alma.init(1,axiom_file, '0', 1, 1000, [], [])
         exp = explosion(explosion_steps)
         res_tasks = exp[0]
         res_lits = res_task_lits(exp[2])
@@ -163,6 +163,7 @@ def test(network_priors, exp_size=10, num_steps=500, alma_heap_print_size=100, p
     return dbb_instances
 
 if __name__ == "__main__":
+    global alma_inst
     parser = argparse.ArgumentParser(description="Run test with specified parameters.")
     parser.add_argument("use_network", type=str)
     parser.add_argument("explosion_steps", type=int, default=10)
@@ -173,8 +174,14 @@ if __name__ == "__main__":
     parser.add_argument("prb_threshold", type=float, default=1.0)
     parser.add_argument("--retrain", action='store', required=False, default="NONE")
     parser.add_argument("--reload", action='store', required=False, default="NONE")
+    parser.add_argument("--axioms", action='store', required=False,
+                        default='/home/justin/alma-2.0/glut_control/test1_kb.pl')
+    parser.add_argument("--num_rounds", action='store', required=False, default="10000")
 
     args = parser.parse_args()
+
+    alma_inst,res = alma.init(1,args.axioms, '0', 1, 1000, [], [])
+    
     use_net = True if args.use_network == "True" else False
     assert(type(use_net) == type(True))
     if args.retrain != "NONE":
@@ -182,12 +189,12 @@ if __name__ == "__main__":
         assert(args.reload == "NONE")
         print("Read network {}, expsteps {}   rsteps {}    model_name {}".format(use_net, args.explosion_steps, args.reasoning_steps, model_name))
         print('Retraining; model name is ', args.retrain)
-        train(args.explosion_steps, args.reasoning_steps, args.numeric_bits, model_name)
+        train(args.explosion_steps, args.reasoning_steps, args.numeric_bits, model_name, args.axioms, int(args.num_rounds))
     if args.reload != "NONE":
         assert(args.retrain == "NONE")
         model_name = args.reload
         print("Read network {}, expsteps {}   rsteps {}    model_name {}".format(use_net, args.explosion_steps, args.reasoning_steps, model_name))
-    
+        
 
     #res = test(use_net, args.explosion_steps, args.reasoning_steps, args.heap_print_size, args.prb_print_size, args.numeric_bits, heap_print_freq=10, model_name = model_name, prb_threshold=0.25)
     res = test(use_net, args.explosion_steps, args.reasoning_steps, args.heap_print_size, args.prb_print_size, args.numeric_bits, heap_print_freq=1, model_name = model_name, prb_threshold=args.prb_threshold)
