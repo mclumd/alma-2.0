5f88abf807306a26cc94f44cafa8698be627ec65
jdbrody <justin.brody@goucher.edu>
2021-02-28 10:16:55 -0500

    fixed typo

--- glut_control/diplomath_go.sh
+++ glut_control/diplomath_go.sh
@@ -1,1 +1,1 @@
-python ./diplomath_testpython ./diplomath_test.py  50 3000 3000 --use_network --train diplomath_model --num_trainings 100 --train_interval 50 --numeric_bits 6 --kb test1_kb.pl
+python ./diplomath_test.py  50 3000 3000 --use_network --train diplomath_model --num_trainings 100 --train_interval 50 --numeric_bits 6 --kb test1_kb.pl
