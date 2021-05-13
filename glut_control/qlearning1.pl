349843196a274973cd923731c160a78bff475861
Justin Brody <jdbrody@gmail.com>
2021-05-13 16:45:17 -0400

    initial ALMA kbs

---
+++ glut_control/qlearning1.pl
@@ -0,0 +1,6 @@
+if(f(X), f(f(X))).
+if(f(X), g(f(X))).
+if(g(X), f(g(X))).
+if(g(X), g(g(X))).
+f(a).
+
---
+++ glut_control/qlearning3.pl
@@ -0,0 +1,5 @@
+loc(a).
+if(loc(X), loc(left(X))).
+if(loc(X), loc(right(X))).
+if(loc(X), loc(up(X))).
+if(loc(X), loc(down(X))).
