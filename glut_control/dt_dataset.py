5edd0688968c867f5cb92bc61af9379daf6ab86d
jdbrody <justin.brody@goucher.edu>
2021-11-28 12:03:39 -0500

    Initial decision transfomer model

---
+++ glut_control/dt_dataset.py
@@ -0,0 +1,237 @@
+import numpy as np
+import random
+from transformers import GPT2Tokenizer
+import torch
+from torch import nn
+
+
+class dt_dataset():
+    def __init__(self, trajectories, state_dim=2048, act_dim=1024, max_ep_len=100, scale=100, device='cpu'):
+        self.trajectories = trajectories 
+        self.num_trajectories = len(trajectories)
+        self.tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
+
+        self.state_dim=state_dim
+        self.act_dim=act_dim
+        self.max_ep_len=max_ep_len
+        self.scale = scale
+        self.device=device
+
+        # We want to be able to handle up to around 200 tokens in a single string; so we'll
+        # embed each token in a vector of length state_dim / 200.
+        # The actions will be generally shorter, so we can limit ourselves to 100.
+
+        self.state_embed = nn.Embedding(self.tokenizer.vocab_size, state_dim // 200)
+        self.action_embed = nn.Embedding(self.tokenizer.vocab_size, act_dim // 100)
+        
+
+    def state_vectorize_list(self, state_str_list):
+        """
+        Given a list of states, return a numpy vector representation of the concatenatation of them.
+
+        Note that for each string s in state_str_list, we'll get a (num_tokens(s),state_dim) embedding
+        of that string.  To get a single embedding of the entire string, we simply flatten it
+        """
+        state_indices = [self.tokenizer(s)['input_ids'] for s in state_str_list]
+        return [ self.state_embed(s).reshape(1,-1) for s in state_indices]
+
+    def state_vectorize_str(self, state_str_list):
+        """
+        Given a state represented as a string, return a numpy vector representation.
+        """
+        state_embeddings = [ self.state_embed( torch.LongTensor(self.tokenizer(s)['input_ids']) ).reshape(1,-1) for s in state_str_list]
+        for i in range(len(state_embeddings)):
+            num_pad = max(0, self.state_dim - state_embeddings[i].shape[1])
+            state_embeddings[i] = torch.cat((state_embeddings[i], torch.zeros(1,num_pad)),dim=1)
+        return torch.stack(state_embeddings, dim=1)
+    
+    def action_vectorize_list(self, action):
+        pass
+
+    def action_vectorize_str(self, action_str_list):
+        action_embeddings =  [ self.action_embed( torch.LongTensor(self.tokenizer(a)['input_ids'])).reshape(1,-1) for a in action_str_list]
+        for i in range(len(action_embeddings)):
+            num_pad = max(0, self.act_dim - action_embeddings[i].shape[1])
+            action_embeddings[i] = torch.cat((action_embeddings[i],torch.zeros(1,num_pad)),dim=1)
+        return torch.stack(action_embeddings, dim=1)
+
+    def get_batch(self, batch_size=256, max_len=128):
+        trajectories = self.trajectories
+
+        batch_inds = np.random.choice(
+            np.arange(self.num_trajectories),
+            size=batch_size,
+            replace=True,
+            #p=p_sample,  # reweights so we sample according to timesteps
+        )
+
+        s, a, r, d, rtg, timesteps, mask = [], [], [], [], [], [], []
+        for i in range(batch_size):
+            traj = trajectories[int(batch_inds[i])]
+
+            si = random.randint(0, traj['total']-1)
+            trunc_traj_kb = traj['kb'][si:si + max_len]
+            trunc_traj_actions = traj['actions'][si:si + max_len]
+            truncated_traj = {'total': len(trunc_traj_kb),
+                              'kb': trunc_traj_kb,
+                              'actions': trunc_traj_actions}
+            #straj = trajectory_unformatted_add_returns(trajectory_unformatted_sar(traj))
+            straj = trajectory_sar_add_returns(sar_format(truncated_traj))
+            states = [x[0] for x in straj]
+            actions = [x[1] for x in straj]
+            rewards = [x[2] for x in straj]
+            returns = [x[4] for x in straj]
+
+            s.append(self.state_vectorize_str(states))
+            a.append(self.action_vectorize_str(actions))
+            r.append(np.array(rewards).reshape(1,-1,1))
+            dones = np.zeros(len(trunc_traj_kb)).reshape(1,-1); dones[0,-1] = 1
+            d.append(dones)
+            # timesteps.append(np.arange(si, si + s[-1].shape[1]).reshape(1, -1))
+            num_timesteps = min(truncated_traj['total'], max_len)
+            timesteps.append(np.arange(si, si + num_timesteps).reshape(1, -1))
+            timesteps[-1][timesteps[-1] >= self.max_ep_len] = self.max_ep_len-1  # padding cutoff
+            rtg.append(np.array(returns).reshape(1,-1,1))
+            if rtg[-1].shape[1] <= num_timesteps:
+                rtg[-1] = np.concatenate([rtg[-1], np.zeros((1, 1,1))], axis=1)
+
+            # padding and state + reward normalization
+            #tlen = s[-1].shape[1]
+            tlen = num_timesteps
+            
+            # Skip normalization for now; requires calculating global stats for datset
+            # s,a padded in vectorization.
+            s[-1] = torch.cat([torch.zeros((1, max_len - tlen, self.state_dim)), s[-1]], axis=1)
+            #s[-1] = (s[-1] - state_mean) / state_std
+            a[-1] = torch.cat([torch.ones((1, max_len - tlen, self.act_dim)) * -10., a[-1]], axis=1)
+            r[-1] = np.concatenate([np.zeros((1, max_len - tlen, 1)), r[-1]], axis=1)
+            d[-1] = np.concatenate([np.ones((1, max_len - tlen)) * 2, d[-1]], axis=1)
+            rtg[-1] = np.concatenate([np.zeros((1, max_len - tlen, 1)), rtg[-1]], axis=1) / self.scale
+            timesteps[-1] = np.concatenate([np.zeros((1, max_len - tlen)), timesteps[-1]], axis=1)
+            mask.append(np.concatenate([np.zeros((1, max_len - tlen)), np.ones((1, tlen))], axis=1))
+
+        #s = torch.from_numpy(np.concatenate(s, axis=0)).to(dtype=torch.float32, device=device)
+        #a = torch.from_numpy(np.concatenate(a, axis=0)).to(dtype=torch.float32, device=device)
+        s = torch.cat(s, axis=0).to(dtype=torch.float32, device=self.device)
+        a = torch.cat(a, axis=0).to(dtype=torch.float32, device=self.device)
+        r = torch.from_numpy(np.concatenate(r, axis=0)).to(dtype=torch.float32, device=self.device)
+        d = torch.from_numpy(np.concatenate(d, axis=0)).to(dtype=torch.long, device=self.device)
+        rtg = torch.from_numpy(np.concatenate(rtg, axis=0)).to(dtype=torch.float32, device=self.device)
+        timesteps = torch.from_numpy(np.concatenate(timesteps, axis=0)).to(dtype=torch.long, device=self.device)
+        mask = torch.from_numpy(np.concatenate(mask, axis=0)).to(device=self.device)
+
+        return s, a, r, d, rtg, timesteps, mask
+
+    def eval_episodes(target_rew):
+            def fn(model):
+                returns, lengths = [], []
+                for _ in range(num_eval_episodes):
+                    with torch.no_grad():
+                        if model_type == 'dt':
+                            ret, length = evaluate_episode_rtg(
+                                env,
+                                state_dim,
+                                act_dim,
+                                model,
+                                max_ep_len=max_ep_len,
+                                scale=scale,
+                                target_return=target_rew/scale,
+                                mode=mode,
+                                state_mean=state_mean,
+                                state_std=state_std,
+                                device=device,
+                            )
+                        else:
+                            ret, length = evaluate_episode(
+                                env,
+                                state_dim,
+                                act_dim,
+                                model,
+                                max_ep_len=max_ep_len,
+                                target_return=target_rew/scale,
+                                mode=mode,
+                                state_mean=state_mean,
+                                state_std=state_std,
+                                device=device,
+                            )
+                    returns.append(ret)
+                    lengths.append(length)
+                return {
+                    f'target_{target_rew}_return_mean': np.mean(returns),
+                    f'target_{target_rew}_return_std': np.std(returns),
+                    f'target_{target_rew}_length_mean': np.mean(lengths),
+                    f'target_{target_rew}_length_std': np.std(lengths),
+                }
+            return fn
+
+# ================================================================================
+# Utilitiy Functions
+# ================================================================================
+def format_kb(kb_list):
+    return "; ".join([x for x in kb_list if "agentname" not in x and "wallnow" not in x])
+
+def format_act(act_list):
+    return "; ".join(act_list)
+
+def reward1(kb):
+    sum=0
+    for s in kb:
+        if ("f(a)" in s) or ("g(a)" in s):
+            sum += s.count("f")
+    return sum
+
+def trajectory_unformatted_sar(trajectory, calc_reward = reward1 ):
+    tkb = trajectory['kb']
+    tact = trajectory['actions']
+    total = trajectory['total']
+    result=[]
+    total_reward = calc_reward(tkb[0]) # The calc_reward gives the total_reward; we subtract the previous
+                                       # reward to get the reward for each indvidual action.
+    for i in range(total):
+        kb = format_kb(tkb[i])
+        act = format_act(tact[i])
+        reward = calc_reward(tkb[i+1]) if i < total-1 else total_reward
+        result.append( (tkb[i], tact[i], reward - total_reward, reward) )
+        total_reward = reward
+    return result
+
+
+def sar_format(trajectory, calc_reward = reward1 ):
+    """
+    Takes a trajectory as input, 
+    returns a list of (state, action, reward) triples as output
+    """
+    tkb = trajectory['kb']
+    tact = trajectory['actions']
+    total = trajectory['total']
+    result=[]
+    total_reward = calc_reward(tkb[0])  # The calc_reward gives the total_reward; we subtract the previous
+    # reward to get the reward for each indvidual action.
+    for i in range(total):
+        kb = format_kb(tkb[i])
+        act = format_act(tact[i])
+        reward = calc_reward(tkb[i+1]) if i < total-1 else total_reward
+        result.append( (kb, act, reward - total_reward, reward) )
+        total_reward = reward
+    return result
+
+def trajectory_sar_add_returns(trajectory_sar):
+    trajectory_total = trajectory_sar[-1][3]
+    return [ (s, a, r, tr, trajectory_total - tr) for (s,a,r,tr) in trajectory_sar ]
+
+def trajectory_unformatted_sar_add_returns(trajectory_sar):
+    trajectory_total = trajectory_sar[-1][3]
+    return [ (s, a, r, tr, trajectory_total - tr) for (s,a,r,tr) in trajectory_sar ]
+
+
+def format_trajectory(trajectory):
+    """
+    A trajectory will be a dictionary with attributes 'total', 'kb',
+    'actions'.
+
+    This will return a list of (state,action,returns_to_go) triples 
+    formatted to be fed into a pre-trained model.
+
+    """
+    sartg_list = trajectory_sar_add_returns(sar_format(trajectory))
+    return [ x[0] + "="*10 + x[1] + ":"*10 + str(x[4]) for x in sartg_list]
---
+++ glut_control/dt_test_train.py
@@ -0,0 +1,125 @@
+import os, sys, argparse
+import wandb
+import pickle
+import numpy as np
+import torch
+from transformers import GPT2Tokenizer
+import dt_dataset
+
+# Find decision_transformer
+cwd = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.join(cwd, "decision-transformer/gym/"))
+
+from decision_transformer.evaluation.evaluate_episodes import evaluate_episode, evaluate_episode_rtg
+from decision_transformer.models.decision_transformer import DecisionTransformer
+from decision_transformer.training.seq_trainer import SequenceTrainer
+
+
+
+
+def train(variant, dataset="offline_datasets/dection_trans_data_qlearning2.1.pkl"):
+    max_ep_len = 20
+    env_targets = [3600, 1800]  # evaluation conditioning targets
+    scale = 1000.  # normalization for rewards/returns
+
+    tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
+
+    # The following was taken from a blog entry; not sure if it can be made to work
+    # with the DecisionTransfofmer class of if it's actually necessary.  It *would* be
+    # nice to limit the vocabulary size!
+    special_tokens_dict = {'bos_token': '<BOS>', 'eos_token': '<EOS>',
+                           'sep_token': '<SEP>'}
+    num_added_toks = tokenizer.add_special_tokens(special_tokens_dict)
+    #model.resize_token_embeddings(len(tokenizer))
+
+    state_dim = 1024
+    act_dim = 512
+    max_length= 128
+    warmup_steps = variant['warmup_steps']
+    with open(dataset, "rb") as dfile:
+        D = pickle.load(dfile)
+        data = dt_dataset.dt_dataset(D, state_dim=state_dim, act_dim=act_dim, max_ep_len=max_ep_len)
+
+    model = DecisionTransformer(
+            state_dim=state_dim,
+            act_dim=act_dim,
+            max_length=max_length,
+            max_ep_len=max_ep_len,
+            hidden_size=variant['embed_dim'],
+            n_layer=variant['n_layer'],
+            n_head=variant['n_head'],
+            n_inner=4*variant['embed_dim'],
+            activation_function=variant['activation_function'],
+            n_positions=1024,
+            resid_pdrop=variant['dropout'],
+            attn_pdrop=variant['dropout'],
+        )
+
+    
+    #model = model.to(device=device)
+    optimizer = torch.optim.AdamW(
+        model.parameters(),
+        lr=variant['learning_rate'],
+        weight_decay=variant['weight_decay'],
+    )
+    scheduler = torch.optim.lr_scheduler.LambdaLR(
+        optimizer,
+        lambda steps: min((steps+1)/warmup_steps, 1)
+    )
+
+
+    trainer = SequenceTrainer(
+            model=model,
+            optimizer=optimizer,
+            batch_size=variant['batch_size'],
+            get_batch=data.get_batch,
+            scheduler=scheduler,
+            loss_fn=lambda s_hat, a_hat, r_hat, s, a, r: torch.mean((a_hat - a)**2),
+            #eval_fns=[eval_episodes(tar) for tar in env_targets],
+        )
+
+    #if log_to_wandb:
+    if False:   #investigate using this later
+        wandb.init(
+            name=exp_prefix,
+            group=group_name,
+            project='decision-transformer',
+            config=variant
+        )
+        # wandb.watch(model)  # wandb has some bug
+
+    for iter in range(variant['max_iters']):
+        outputs = trainer.train_iteration(num_steps=variant['num_steps_per_iter'], iter_num=iter+1, print_logs=True)
+        if log_to_wandb:
+            wandb.log(outputs)        
+
+def test():
+    pass
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--env', type=str, default='hopper')
+    parser.add_argument('--dataset', type=str, default='medium')  # medium, medium-replay, medium-expert, expert
+    parser.add_argument('--mode', type=str, default='normal')  # normal for standard setting, delayed for sparse
+    parser.add_argument('--K', type=int, default=20)
+    parser.add_argument('--pct_traj', type=float, default=1.)
+    parser.add_argument('--batch_size', type=int, default=64)
+    parser.add_argument('--model_type', type=str, default='dt')  # dt for decision transformer, bc for behavior cloning
+    parser.add_argument('--embed_dim', type=int, default=128)
+    parser.add_argument('--n_layer', type=int, default=3)
+    parser.add_argument('--n_head', type=int, default=1)
+    parser.add_argument('--activation_function', type=str, default='relu')
+    parser.add_argument('--dropout', type=float, default=0.1)
+    parser.add_argument('--learning_rate', '-lr', type=float, default=1e-4)
+    parser.add_argument('--weight_decay', '-wd', type=float, default=1e-4)
+    parser.add_argument('--warmup_steps', type=int, default=10000)
+    parser.add_argument('--num_eval_episodes', type=int, default=100)
+    parser.add_argument('--max_iters', type=int, default=10)
+    parser.add_argument('--num_steps_per_iter', type=int, default=10000)
+    parser.add_argument('--device', type=str, default='cuda')
+    parser.add_argument('--log_to_wandb', '-w', type=bool, default=False)
+    
+    args = parser.parse_args()
+
+    train(variant=vars(args))
