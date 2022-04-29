27099a539df0d99dfab6d5c94bbdd53135189b73
Matt Goldberg <goldburger@users.noreply.github.com>
2022-04-28 23:41:54 -0400

    Some file removal, others renamed

--- demo/default/car/almafiles/ford_prius2.pl
+++
@@ -1,7 +0,0 @@
-rel(gas_tank_car, ford).
-not(rel(fueled, ford)).
-
-fif(now(4), rel(electric_car, prius)).
-fif(now(4), rel(gas_tank_car, prius)).
-fif(now(4), rel(charged, prius)).
-fif(now(4), not(rel(fueled, prius))).
